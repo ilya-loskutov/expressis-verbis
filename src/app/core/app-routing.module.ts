b588b12af9d540143fcb3e43cce7b2494359cf9c
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-05-04 12:18:06 +0300

    Return the routing module back to using the entry component instead of the temporary entry component

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DictionaryOptionsComponent } from '../dictionary/components/dictionary-options/dictionary-options.component';
-//import { EntryComponent } from '../entry/components/entry/entry.component';
-import { TemporaryEntryComponent } from '../entry/components/temporary-entry/temporary-entry.component';
+import { EntryComponent } from '../entry/components/entry/entry.component';
 import { EntryListComponent } from '../entry/components/entry-list/entry-list.component';
 import { navigationPaths } from '../shared/config/navigation-paths/navigation-paths';
 import { SelectedDictionaryGuard } from './services/selected-dictionary.guard';
@@ -26,14 +25,14 @@ const routes: Routes = [
   },
   {
     path: navigationPaths.newEntry,
-    component: /* EntryComponent */ TemporaryEntryComponent,
+    component: EntryComponent,
     canActivate: [SelectedDictionaryGuard],
     resolve: { entry: EntryResolver },
     canDeactivate: [CanDeactivateEntryGuard]
   },
   {
     path: navigationPaths.entry,
-    component: /* EntryComponent */ TemporaryEntryComponent,
+    component: EntryComponent,
     canActivate: [SelectedDictionaryGuard],
     resolve: { entry: EntryResolver },
     canDeactivate: [CanDeactivateEntryGuard]
