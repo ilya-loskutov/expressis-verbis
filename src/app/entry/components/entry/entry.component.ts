4119cdf6d75d1d1ab71a07a89b11a389cd8350cd
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-05-04 18:15:21 +0300

    Add functionality to submit form to the entry component

--- src/app/entry/components/entry/entry.component.ts
+++ src/app/entry/components/entry/entry.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder } from '@angular/forms';
 import { map, Subscription, Subject } from 'rxjs';
 
 import { Entry } from '../../models/entry';
+import { EntryService } from '../../services/entry.service';
 import { EntryForm } from '../../models/entry-form';
 import { entryWordsValidator, entryMeaningsValidator } from '../../services/entry-form-control-validators';
 import { ButtonState } from 'src/app/shared/config/components/button';
@@ -22,6 +23,7 @@ export class EntryComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private formBuilder: FormBuilder,
+    private entryService: EntryService
   ) { }
 
   ngOnInit(): void {
@@ -59,11 +61,50 @@ export class EntryComponent implements OnInit, OnDestroy {
     this.entrySubscription.unsubscribe();
   }
 
-  get isEntryChanged(): boolean {
-    return this.entryForm.dirty;
+  async onFormSubmit(): Promise<void> {
+    if (this.entryForm.invalid) {
+      this.onInvalidFormSubmit();
+    }
+    else {
+      await this.onValidFormSubmit();
+    }
+  }
+
+  private onInvalidFormSubmit(): void {
+    this.attemptsToSubmitInvalidForm$.next();
+    if (this.entryForm.errors!['entryWordsLengthLessThanAllowableValueError']) {
+      this.scrollTo('words');
+    }
+    else if (this.entryForm.errors!['someMeaningsAreBeingEdited']) {
+      this.scrollTo('meanings');
+    }
+    else {
+      throw new Error(`Unexpected form errors: ${JSON.stringify(this.entryForm.errors)}`)
+    }
   }
 
   attemptsToSubmitInvalidForm$: Subject<void> = new Subject<void>();
+
+  private scrollTo(elementId: string): void {
+    const entryWordsElement = document.getElementById(elementId) as HTMLElement;
+    entryWordsElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  private async onValidFormSubmit(): Promise<void> {
+    this.entryForm.disable();
+    await this.entryService.addOrUpdate(this.entryForm.value as Entry);
+    this.entryForm.markAsPristine();
+    this.router.navigate([navigationPaths.entryList]);
+  }
+
+  get isSubmitFormButtonDisabled(): boolean {
+    return this.entryForm.disabled ||
+      !this.isEntryChanged;
+  }
+
+  get isEntryChanged(): boolean {
+    return this.entryForm.dirty;
+  }
 
   cancelFormSubmission(): void {
     this.router.navigate([navigationPaths.entryList]);
